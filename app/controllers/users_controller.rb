225a65c7881ed05422f6fc07b9864d83039ce1e6
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-09 02:32:49 +0900

    エラーメッセージを表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,19 +23,30 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(user_params)
 
-    respond_to do |format|
+  # def create
+  #   @user = User.new(user_params)
+  #
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  def create
+      @user = User.new(user_params)
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        # 保存の成功した場合の処理
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
     end
-  end
+
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -69,6 +80,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
     end
 end
