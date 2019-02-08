ef2eda3ad0cc7d462e9732e01c04884aa73839ca
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-09 02:47:53 +0900

    ユーザー詳細ページの作成と登録後のリダイレクトの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -23,25 +24,10 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-
-  # def create
-  #   @user = User.new(user_params)
-  #
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   def create
       @user = User.new(user_params)
       if @user.save
-        # 保存の成功した場合の処理
+        redirect_to user_path(@user.id)
       else
         render 'new'
       end
