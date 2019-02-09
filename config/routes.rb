43d304925bc78291a25a80465f9d5e3c59cac231
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-09 11:12:19 +0900

    ログインフォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 end
