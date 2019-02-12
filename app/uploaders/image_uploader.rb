37c2ea28667b0a933720d369bc755f377015ce84
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-12 20:37:11 +0900

    Bootstrapで画面の編集をした

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  process :resize_to_limit => [200, 200]
+  process :resize_to_limit => [300, 300]
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
