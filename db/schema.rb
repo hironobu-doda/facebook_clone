d758664295f5d6faad12fe9346494dde82cb53fa
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-12 21:37:20 +0900

    feedカラムを削除するというマイグレ-ションファイルを作成し、feedカラムを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211015222) do
+ActiveRecord::Schema.define(version: 20190212123105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,6 @@ ActiveRecord::Schema.define(version: 20190211015222) do
     t.text "image"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_blogs_on_user_id"
-  end
-
-  create_table "feeds", force: :cascade do |t|
-    t.text "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
